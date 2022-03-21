f35bb81a5aa0678de37d09af05093a56330cb1ff
xinulsw <xinulsw@gmail.com>
2022-03-21 11:21:12 +0100

    Uzupełnienie kodu sprawdzającego zainstalowane moduły.

--- Rozdzial_5/r5_01_data_frame.py
+++ Rozdzial_5/r5_01_data_frame.py
@@ -1,15 +1,17 @@
 # program r5_01_data_frame.py
 # pandas
 
-from sys import exit
+import sys
 
 try:
     import pandas as pd
-
     print("Moduł pandas wczytany.")
-except:
-    print("Zainstaluj: 'pip install pandas' ")
-    exit(0)
+    import matplotlib.pyplot as plt
+    print("Moduł matplotlib obecny.")
+except ImportError as impErr:
+    print("Brak modułu", impErr.args[0][16:].replace("'", ""))
+    print("Zainstaluj: pip install", impErr.args[0][16:].replace("'", ""))
+    sys.exit(0)
 
 # Jest ok, działamy dalej
 source_data = "http://otwartedane.gdynia.pl/portal/data/city/6/3/data.csv"
