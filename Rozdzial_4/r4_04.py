6dcf00046b2c66d884b7e9529ed366da9f2cc563
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2022-08-28 21:48:25 +0200

    changing imports

--- Rozdzial_4/r4_04.py
+++ Rozdzial_4/r4_04.py
@@ -5,10 +5,51 @@
 
 from sys import exit
 from r4_functions import *
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from datetime import date, datetime, timedelta
-import numpy as np
+
+load_module_ok = True
+
+try:
+    import numpy as np
+
+    ok_module_info("numpy")
+except:
+    error_module_info("numpy")
+    load_module_ok = False
+
+try:
+    import matplotlib.pyplot as plt
+    import matplotlib.animation as animation
+
+    ok_module_info("matplotlib")
+except:
+    err_module_info("matplotlib")
+    load_module_ok = False
+
+try:
+    from astropy.time import Time
+
+    ok_module_info("astropy")
+except:
+    error_module_info("astropy")
+    load_module_ok = False
+
+try:
+    from astroquery.jplhorizons import Horizons
+
+    ok_module_info("astroquery")
+except:
+    error_module_info("astroquery")
+    load_module_ok = False
+
+if not load_module_ok:
+    print("Niestety, wystąpiły błędy.")
+    print("Nie mogę dalej działać.")
+    exit(0)
+
+# Teraz mamy wszystkie moduły zainstalowane
+print("Super! Możemy działać....")
+
+from datetime import datetime, timedelta
 
 
 class CosmicObject:  # Definiujemy poszczególne obiekty kosmiczne (Słońce, Ziemia,...)
@@ -77,49 +118,6 @@ class SolarSystem:
         # Zwracamy wartości niezbędne do wygenerowania animacji
         return plots + lines + [self.timestamp]
 
-
-load_module_ok = True
-
-try:
-    import numpy as np
-
-    ok_module_info("numpy")
-except:
-    error_module_info("numpy")
-    load_module_ok = False
-
-try:
-    import matplotlib
-
-    ok_module_info("matplotlib")
-except:
-    err_module_info("matplotlib")
-    load_module_ok = False
-
-try:
-    from astropy.time import Time
-
-    ok_module_info("astropy")
-except:
-    error_module_info("astropy")
-    load_module_ok = False
-
-
-try:
-    from astroquery.jplhorizons import Horizons
-
-    ok_module_info("astroquery")
-except:
-    error_module_info("astroquery")
-    load_module_ok = False
-
-if not load_module_ok:
-    print("Niestety, wystąpiły błędy.")
-    print("Nie mogę dalej działać.")
-    exit(0)
-
-# Teraz mamy wszystkie moduły zainstalowane
-print("Super! Możemy działać....")
 
 nasaids = [1, 2, 3, 4]  # numery ID w bazie NASA
 names = ["Merkury", "Wenus", "Ziemia", "Mars"]
